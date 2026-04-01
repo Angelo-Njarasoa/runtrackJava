4877d48c5452228cbdf214eb506c841e6ea773d3
andoniaina-angelo <andoniaina.njarasoa@laplateforme.io>
2026-04-01 15:14:08 +0200

    add job2

---
+++ jour1/jour2.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class jour2
+{public static void main(String[] args){
+ Scanner scanner =new Scanner(System.in );
+ System.out.println("écrivez votrre prenom");
+ String prenom = scanner.nextLine();
+ System.out.println("bonjour " + prenom);
+}
+}
