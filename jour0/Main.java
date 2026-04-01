af310e45bbc54e1dafa97704b55621d1b1d43c24
andoniaina-angelo <andoniaina.njarasoa@laplateforme.io>
2026-04-01 10:43:35 +0200

    add jour 0

---
+++ jour0/Main.java
@@ -0,0 +1,14 @@
+public class Main 
+{public static void main(String [] args){int age = 10 ;
+String phrase = "hello world " ;
+  long nb = 1995 ;
+  float flt = 1.6F;
+  char alpha ='g' ; 
+  boolean realite = true;
+System.out.println(phrase);
+System.out.println(nb);
+System.out.println(flt);
+System.out.println(age);
+System.out.println(realite);
+}
+}
