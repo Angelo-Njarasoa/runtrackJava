1c543167895476f69fa51d960d4e6489dde0cdfc
andoniaina-angelo <andoniaina.njarasoa@laplateforme.io>
2026-04-01 17:04:09 +0200

    add job 2 and 3

---
+++ jour1/job3.java
@@ -0,0 +1,9 @@
+public class jour3
+{ public static void main(String [] args){
+    int[] T= {0,1,2,3,4,5,6,7,8,9};
+System.out.println("valeur de l'index 0 =" + T[0]);
+System.out.println("valeur de l'index 1 =" + T[1]);
+System.out.println("valeur de l'index 5 =" + T[5]);
+System.out.println("valeur de l'index 9 =" + T[9]);
+System.out.println("T[10] n'existe pas (indice hors limite)");
+}}
