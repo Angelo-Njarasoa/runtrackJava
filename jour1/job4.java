50a713688410780c67c1cc726161c00c29172ae3
andoniaina-angelo <andoniaina.njarasoa@laplateforme.io>
2026-04-02 14:40:04 +0200

    add job4

---
+++ jour1/job4.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class job4 
+{
+public static void main( String[] args)
+{Scanner scanner = new Scanner(System.in);
+    System.out.println("nb a calculer :");
+    double nb = scanner.nextInt();
+    System.out.println("resultat du nb au carré " + nb*nb);
+
+}
+}
