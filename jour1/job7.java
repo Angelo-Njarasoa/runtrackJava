a8a1083fa936dcff165d02eb5d1ab2316aaf9029
andoniaina-angelo <andoniaina.njarasoa@laplateforme.io>
2026-04-08 01:22:22 +0200

    add job 7

---
+++ jour1/job7.java
@@ -0,0 +1,12 @@
+public class job7
+{public static void main( String [] args)
+    {
+        int v=0;
+        int i;
+        for(i=1;i<=100;i++)
+        {v= v+i;}
+        System.out.println("la somme des nombres des 100 premierentier est "+ v);
+
+
+    }
+}
