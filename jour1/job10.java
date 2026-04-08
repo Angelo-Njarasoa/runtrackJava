314bb3e81d3c7c4e4058b0f70068d6d94be9b5f9
andoniaina-angelo <andoniaina.njarasoa@laplateforme.io>
2026-04-08 04:42:50 +0200

    add job 10

---
+++ jour1/job10.java
@@ -0,0 +1,13 @@
+public class job10
+{
+    public static void main (String [] args)
+    {
+        int b = 8;
+        int i ;
+        int fact=1;
+        for (i=1; i<=b; i++)
+        {fact=fact * i;}
+        System.out.println("le factoriel de b est "+fact);
+
+    }
+}
